dddfc9c36e0fb86d8ba4cad1e3a2e93edca7afd5
Sparkle Party <lexi.slovik@gmail.com>
2022-06-03 16:03:25 -0700

    database connections and close, filters to format templates

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -14,3 +15,24 @@ engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
 Session = sessionmaker(bind=engine)
 # helps us map the models to real MySQL tables.
 Base = declarative_base()
+
+def init_db(app):
+    Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
+
+# returns a new session-connection object whenever this function is called
+def get_db():
+    # saves the current connection on the g object, if it's not already there
+    if  'db' not in g:
+        # store connection in app context
+        g.db = Session()
+    # returns the connection from the g object instead of creating a new Session instance each time
+    return g.db
+
+def close_db(e=None):
+    # attempts to find and remove db from the g object
+    db = g.pop('db', None)
+    # If db exists (that is, db doesn't equal None), then db.close() will end the connection
+    if db is not None:
+        db.close()
--- app/routes/home.py
+++ app/routes/home.py
@@ -2,13 +2,19 @@ from flask import Blueprint, render_template
 # Blueprint() lets us consolidate routes onto a single bp object 
 # that the parent app can register later. 
 # This corresponds to using the Router middleware of Express.js.
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    # whatever the function returns becomes the response
-    return render_template('homepage.html')
+    # get all posts
+    # returns a session connection that's tied to this route's context
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
+    return render_template('homepage.html', posts=posts)
 
 @bp.route('/login')
 def login():
@@ -16,4 +22,9 @@ def login():
 
 @bp.route('/post/<id>')
 def single(id):
-    return render_template('single-post.html')
+    # get single post by id
+    db = get_db()
+    # use the filter() method on the connection object to specify the SQL WHERE clause
+    post = db.query(Post).filter(Post.id == id).one()
+
+    return render_template('single-post.html', post=post)
