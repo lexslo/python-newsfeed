be3b3e9a9328c77f806faba163baf3a2714fe627
Sparkle Party <lexi.slovik@gmail.com>
2022-06-12 18:34:00 -0700

    add CRUD routes, protect routes

---
+++ app/utils/auth.py
@@ -0,0 +1,19 @@
+from flask import session, redirect
+from functools import wraps
+
+# expects to receive another function as an argument
+def login_required(func):
+    # preserves the original name when creating the wrapped function
+    @wraps(func)
+    # *args and **kwargs keywords ensure that no matter how many arguments are given (if any), 
+    # the wrapped_function() captures them all.
+    def wrapped_function(*args, **kwargs):
+        # if logged in, call original function with original arguments
+        if session.get('loggedIn') == True:
+          return func(*args, **kwargs)
+
+        return redirect('/login')
+
+    return wrapped_function
+
+
